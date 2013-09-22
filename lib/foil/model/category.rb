06cf1948f7d3a3aa5e43af9c42a55cf67f722089
Jamie Hodge <jamiehodge@me.com>
2013-09-22 21:50:40 +0200

    add static caching

--- lib/foil/model/category.rb
+++ lib/foil/model/category.rb
@@ -4,6 +4,7 @@ module Foil
   module Model
     class Category < Base
       plugin :rcte_tree
+      plugin :static_cache
       many_to_many :files
     end
   end
--- lib/foil/model/language.rb
+++ lib/foil/model/language.rb
@@ -3,6 +3,7 @@ require_relative 'base'
 module Foil
   module Model
     class Language < Base
+      plugin :static_cache
       many_to_many :files
     end
   end
