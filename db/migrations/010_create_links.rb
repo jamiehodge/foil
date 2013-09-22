33b2ed93cdc478307a2b2fb0d21980185f521483
Jamie Hodge <jamiehodge@me.com>
2013-09-22 21:48:01 +0200

    add links and tags to utterances (reorder migrations)

--- db/migrations/010_create_links.rb
+++ db/migrations/010_create_links.rb
@@ -1,21 +1,22 @@
 Sequel.migration do
   change do
-    create_table :tags do
+    create_table :links do
       primary_key :id
 
-      text :name, null: false, unique: true
+      text :url, null: false, unique: true
 
       timestamp :created_at, default: Sequel::CURRENT_TIMESTAMP, null: false
       timestamp :updated_at, default: Sequel::CURRENT_TIMESTAMP, null: false
 
       integer :lock_version, default: 0, null: false
 
-      full_text_index :name
+      full_text_index :url
     end
 
-    create_trigger :tags, :timestamp, :moddatetime,
+    create_trigger :links, :timestamp, :moddatetime,
       args: :updated_at, each_row: true, events: :update
 
-    create_join_table file_id: :files, tag_id: :tags
+    create_join_table file_id: :files, link_id: :links
+    create_join_table link_id: :links, utterance_id: :utterances
   end
 end
--- db/migrations/011_create_tags.rb
+++ db/migrations/011_create_tags.rb
@@ -1,21 +1,22 @@
 Sequel.migration do
   change do
-    create_table :links do
+    create_table :tags do
       primary_key :id
 
-      text :url, null: false, unique: true
+      text :name, null: false, unique: true
 
       timestamp :created_at, default: Sequel::CURRENT_TIMESTAMP, null: false
       timestamp :updated_at, default: Sequel::CURRENT_TIMESTAMP, null: false
 
       integer :lock_version, default: 0, null: false
 
-      full_text_index :url
+      full_text_index :name
     end
 
-    create_trigger :links, :timestamp, :moddatetime,
+    create_trigger :tags, :timestamp, :moddatetime,
       args: :updated_at, each_row: true, events: :update
 
-    create_join_table file_id: :files, link_id: :links
+    create_join_table file_id: :files, tag_id: :tags
+    create_join_table tag_id:  :tags, utterance_id: :utterances
   end
 end
